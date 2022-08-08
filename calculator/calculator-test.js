4373507a6e55d519bb78500da965ba2991f342df
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2022-08-08 19:21:34 -0400

    Added tests for calculating monthly rate, also for checking for decimal in ccalculation result

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,20 @@
+describe('calculator tests', () => {
+  it('should calculate the monthly rate correctly', function () {
+    const values = {
+      amount: 5000,
+      years: 10,
+      rate: 5,
+    };
+   expect(calculateMonthlyPayment(values)).toEqual('53.03')
+  });
+  it("should return a result with 2 decimal places", function() {
+    const values = {
+      amount: 5000,
+      years: 10,
+      rate: 5,
+    };
+    expect(calculateMonthlyPayment(values)).toContain('.');
+  });
+})
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
-});
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
