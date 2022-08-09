1cd712d469f8c6374d6e0c3deca7db373bc59271
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2022-08-09 19:37:44 -0400

    finished tests for payments functions

--- tip-pool/payments.test.js
+++ tip-pool/payments.test.js
@@ -1,18 +1,49 @@
 describe('Test for submiting payment info', () => {
     beforeEach(() => {
-        serverNameInput.value = 'Alice';
         billAmtInput.value = 50;
         tipAmtInput.value = 5;
-    })
-    it('should update payment id', () => {
-        createCurPayment();
-        submitPaymentInfo();
+    });
+    it('should update allPayments keys values', () => {
+        submitPaymentInfo()
 
-        expect(paymentId).toEqual(1);
-        expect(Object.values(allPayments)).toEqual([Object({
+        expect(Object.keys(allPayments).length).toEqual(1);
+        expect(allPayments['payment1'].billAmt).toEqual('50');
+        expect(allPayments['payment1'].tipAmt).toEqual('5');
+        expect(allPayments['payment1'].tipPercent).toEqual(10);
+        
+    });
+    it('should append current payment values to table', () => {
+        let curPayment = createCurPayment();
+
+        allPayments['payment1'] = curPayment;
+        appendPaymentTable(curPayment);
+
+        let curTdList = document.querySelectorAll('#paymentTable tbody tr td');
+        
+        expect(curTdList.length).toEqual(3);
+        expect(curTdList[0].innerText).toEqual('$50');
+        expect(curTdList[1].innerText).toEqual('$5');
+        expect(curTdList[2].innerText).toEqual('10%');
+    })
+    it('should update bill,tip and tip percent amounts', () => {
+        const amount= {
             billAmt: '50',
             tipAmt: '5',
             tipPercent: 10
-        })]);
+        };
+
+        expect(createCurPayment()).toEqual(amount)
     })
+    
+    afterEach(function() {
+        billAmtInput.value = '';
+        tipAmtInput.value = '';
+        paymentTbody.innerHTML = '';
+        summaryTds[0].innerHTML = '';
+        summaryTds[1].innerHTML = '';
+        summaryTds[2].innerHTML = '';
+        serverTbody.innerHTML = '';
+        paymentId = 0;
+        allPayments = {};
+      });
 })
