1afbb9c61cdc0205102e2f387eca05a0e8d4eb5b
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2022-08-08 20:40:39 -0400

    Added tests for updateServerTable function and submitServerInfo function

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -12,6 +12,41 @@ describe("Servers test (with setup and tear-down)", function() {
   });
 
   afterEach(function() {
-    // teardown logic
+    serverNameInput.value = '';
+    allServers = {};
+    serverId = 0;
+    serverTbody.innerHTML = '';
   });
 });
+
+describe('Update server table test', () => {
+  beforeEach(() => {
+    //initialization logic
+    serverNameInput.value = 'Alice';
+    billAmtInput.value = 50;
+    tipAmtInput.value = 5;
+  })
+  it('should update the server table and calculate how much that server has made', () => {
+    submitServerInfo();
+    submitPaymentInfo();
+
+    expect(Object.keys(allPayments).length).toEqual(1);
+    expect(sumPaymentTotal('tipAmt') / Object.keys(allServers).length).toEqual(5.00)
+  })
+  afterEach(() => {
+    //Clears server input
+    serverNameInput.value = '';
+    //Clears servers array and payments array
+    allServers = {};
+    allPayments = {}
+    //Clears ID #'s
+    serverId = 0;
+    paymentId = 0;
+    //Clears table HTML
+    serverTbody.innerHTML = '';
+    paymentTbody.innerHTML = '';
+    summaryTds[0].innerHTML = '';
+    summaryTds[1].innerHTML = '';
+    summaryTds[2].innerHTML = '';
+  })
+})
